6093e1e618e8ca2494756c7abc04fa74ee3c5a01
Munny <munnydevmaster@gmail.com>
2024-11-18 10:35:08 +0500

    sec commit

--- script.js
+++ script.js
@@ -71,8 +71,8 @@ function applyColorUser2() {
         alert('Please Select a Color For CPU');
     }
 }
-let err = document.getElementById("err")
 let ch = 0
+let err = document.getElementById("err")
 function chColors() {
     var user1Color = document.querySelector('input[name="color1"]:checked').value;
     var user2Color = document.querySelector('input[name="color2"]:checked').value;
@@ -114,6 +114,7 @@ function mainGame() {
     mainsound.pause()
 }
 
+import boxes from "./boxes.js"
 let face11 = document.getElementById("1face1")
 let face12 = document.getElementById("1face2")
 let face13 = document.getElementById("1face3")
@@ -121,10 +122,8 @@ let face14 = document.getElementById("1face4")
 let face15 = document.getElementById("1face5")
 let face16 = document.getElementById("1face6")
 let faces1 = [face11, face12, face13, face14, face15, face16]
-import boxes from "./boxes.js"
-let box1 = document.getElementById("gotiya1")
-
 let open1 = 0
+let previousEle1 = null
 rollbtn1.addEventListener("click", function () {
     vid1.play()
     vid1.style.display = 'flex'
@@ -133,20 +132,27 @@ rollbtn1.addEventListener("click", function () {
     rollbtn1.style.display = "none"
     let num1 = Math.floor(Math.random() * 6);
     let newnum1 = num1 + 1
-    console.log(newnum1);
     setTimeout(() => {
         faces1[num1].style.display = 'grid'
         vid1.style.display = 'none'
+        let newele1 = document.createElement("div")
+        newele1.className = "fa-solid fa-chess-pawn"
+        newele1.style.marginBottom = ".5vw"
+        newele1.style.color = selectedColorGlobal1
         if ((open1 == 0 && newnum1 == 1) || (open1 == 0 && newnum1 == 6)) {
-            console.log("Hello:)");
             user1.style.display = "none"
-            let newele1 = document.createElement("div")
-            newele1.className = "fa-solid fa-chess-pawn"
-            newele1.style.marginBottom = ".5vw"
-            newele1.style.color = selectedColorGlobal1
             boxes[1 - 1].appendChild(newele1)
             open1 = 1
         }
+        else if (open1 == 1) {
+            if (previousEle1 && previousEle1.parentNode) {
+                previousEle1.parentNode.removeChild(previousEle1);
+            }
+            console.log(newnum1);
+            console.log(boxes[newnum1]);
+            boxes[newnum1].appendChild(newele1)
+        }
+        previousEle1 = newele1
         setTimeout(() => {
             vid2.style.display = "flex"
             rollbtn2.style.display = "flex"
@@ -164,8 +170,8 @@ let face24 = document.getElementById("2face4")
 let face25 = document.getElementById("2face5")
 let face26 = document.getElementById("2face6")
 let faces2 = [face21, face22, face23, face24, face25, face26]
-
 let open2 = 0
+let previousEle2 = null
 rollbtn2.addEventListener("click", function () {
     vid2.play()
     vid2.style.display = 'flex'
@@ -174,20 +180,27 @@ rollbtn2.addEventListener("click", function () {
     rollbtn2.style.display = "none"
     let num2 = Math.floor(Math.random() * 6);
     let newnum2 = num2 + 1
-    console.log(newnum2);
     setTimeout(() => {
         faces2[num2].style.display = 'grid'
         vid2.style.display = 'none'
+        let newele2 = document.createElement("div")
+        newele2.className = "fa-solid fa-chess-pawn"
+        newele2.style.marginBottom = ".5vw"
+        newele2.style.color = selectedColorGlobal2
         if ((open2 == 0 && newnum2 == 1) || (open2 == 0 && newnum2 == 6)) {
-            console.log("Hello:)");
             user2.style.display = "none"
-            let newele2 = document.createElement("div")
-            newele2.className = "fa-solid fa-chess-pawn"
-            newele2.style.marginBottom = ".5vw"
-            newele2.style.color = selectedColorGlobal2
             boxes[1 - 1].appendChild(newele2)
             open2 = 1
         }
+        else if (open2 == 1) {
+            if (previousEle2 && previousEle2.parentNode) {
+                previousEle2.parentNode.removeChild(previousEle2);
+            }
+            console.log(newnum2);
+            console.log(boxes[newnum2]);
+            boxes[newnum2].appendChild(newele2)
+        }
+        previousEle2 = newele2
         setTimeout(() => {
             vid1.style.display = "flex"
             rollbtn1.style.display = "flex"
@@ -198,8 +211,8 @@ rollbtn2.addEventListener("click", function () {
     }, 999);
 })
 
-let container = document.getElementById("cont2")
 let gg = 0
+let container = document.getElementById("cont2")
 setInterval(() => {
     if (open1 == 1 && open2 == 1 && gg == 0) {
         container.style.paddingBottom = "4vw"
